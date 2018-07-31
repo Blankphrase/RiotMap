836c83a3feb40a6807664754efb85c3f9642b07e
Blankphrase <ckasera6486@gmail.com>
2018-07-31 18:30:49 +0300

    register models with the admin

--- riot/admin.py
+++ riot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from riot.models import Places, Profile, RiotPronePlaces, NowRioting
 
 # Register your models here.
+
+admin.site.register(RiotPronePlaces)
+admin.site.register(NowRioting)
+admin.site.register(Profile)
+admin.site.register(Places)
