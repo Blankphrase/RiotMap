f117e8ca708946ebc66ac30329baa512401ba4dc
Blankphrase <ckasera6486@gmail.com>
2018-07-31 17:04:23 +0300

    creation of important places model

--- riot/models.py
+++ riot/models.py
@@ -1,9 +1,23 @@
-from django.db import models
+from  django.contrib.auth.models import User
+from django.contrib.gis.db import models
 
 # Create your models here.
 
+class Places(models.Model):
+    """
+    Model for an important place
+    """
+    name = models.CharField(max_length=200)
+    location = models.PointField()
 
-class Profile(models.Model):
-    user = models.OneToOneField(User)
-    name = models.CharField(max_length = 30)
-    email = models.EmailField()
+
+class Important(models.Model):
+    """
+    Model for an event
+    """
+    name = models.CharField(max_length=200)
+    datetime = models.DateTimeField()
+    place = models.ForeignKey(Places)
+
+
+
