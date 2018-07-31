3b9081665fa8ac2b57538fd2fa36b700b88503aa
Blankphrase <ckasera6486@gmail.com>
2018-07-31 18:30:14 +0300

    creation of fields for the models

--- riot/models.py
+++ riot/models.py
@@ -10,27 +10,43 @@ class Places(models.Model):
     name = models.CharField(max_length=200)
     location = models.PointField()
 
+    def __str__(self):
+        return self.name
+
 
 class Profile(models.Model):
     """
     Model for an important place
     """
     user = models.OneToOneField(User)
-    locationName = models.CharField(max_length=200)
     datetime = models.DateTimeField()
     place = models.ForeignKey(Places)
+
+
+    def __str__(self):
+        return "%s" % (self.place.name)
+
+
 
 class RiotPronePlaces(models.Model):
     """
     Model for an area where riot has occured
     """
-    name = models.CharField(max_length=200)
-    riotProne = models.ForeignKey(Places)
+    place = models.ForeignKey(Places)
+
+
+    def __str__(self):
+        return "%s" % (self.place.name)
+
+
 
 class NowRioting(models.Model):
     """
     Model for an area where a riot is happening
     """
-    name = models.CharField(max_length=200)
-    riotingNow = models.ForeignKey(Places)
+    place = models.ForeignKey(Places)
+
+    def __str__(self):
+        return "%s" % (self.place.name)
+
 
