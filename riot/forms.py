563cb7a995038578693609a34ffc35818a0f0a6f
Blankphrase <ckasera6486@gmail.com>
2018-07-31 16:56:46 +0300

    modeling the signup form

---
+++ riot/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password=forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model=User
+        fields=('username','email','password')
+
+    def clean(self):
+        cleaned_data = super(SignUpForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            ) 
+
+
