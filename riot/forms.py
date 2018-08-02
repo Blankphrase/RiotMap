24a9252638b81f7b7c02bfc4a0088ac19224c847
Blankphrase <ckasera6486@gmail.com>
2018-08-02 10:10:44 +0300

    creation of form widgets for various locations

--- riot/forms.py
+++ riot/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from floppyforms.gis import PointWidget, BaseGMapWidget
+from riot.models import Places, Profile, RiotPronePlaces, NowRioting
+
+class CustomPointWidget(PointWidget, BaseGMapWidget):
+    class Media:
+        js = ('/static/floppyforms/js/MapWidget.js',)
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(label='First Name',max_length=30)
@@ -20,5 +27,37 @@ class SignUpForm(UserCreationForm):
             raise forms.ValidationError(
                 "password and confirm_password does not match"
             ) 
+
+class PlacesForm(ModelForm):
+    class Meta:
+        model = Places
+        fields = ['name', 'location']
+        widgets = {
+            'location': CustomPointWidget()
+        }
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'place']
+        widgets = {
+            'place': CustomPointWidget()
+        }
+
+class ProneForm(ModelForm):
+    class Meta:
+        model = RiotPronePlaces
+        fields = ['place']
+        widgets = {
+            'place': CustomPointWidget()
+        }
+
+class NowForm(ModelForm):
+    class Meta:
+        model = NowRioting
+        fields = [ 'place']
+        widgets = {
+            'place': CustomPointWidget()
+        }
 
 
